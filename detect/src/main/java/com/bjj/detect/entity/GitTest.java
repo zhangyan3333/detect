c5ab918f16fe10c63ce8792da88c5de158b4c785
“zhangyan” <2637647135@qq.com>
2024-07-07 20:43:22 +0800

    新增GitTest

---
+++ detect/src/main/java/com/bjj/detect/entity/GitTest.java
@@ -0,0 +1,12 @@
+package com.bjj.detect.entity;
+
+import com.syzx.framework.entity.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GitTest extends AbstractEntity {
+
+	private String message;
+}
